d313f17229e7e7418f0d79d4a43fae75dfd9b6da
DESKTOP-PE8APUQ\ScorpionIPX <scorpionipx@gmail.com>
2017-10-21 13:31:20 +0300

    Created bat script for uninstalling python module

---
+++ venv/uninstall_python_module.bat
@@ -0,0 +1,37 @@
+@echo off
+color b
+cls
+
+Scripts\python -V
+echo.
+
+GOTO HELP
+
+:UNINSTALL_MODULE
+echo Enter module's name!
+set /P module_name=module: 
+
+if %module_name% == ex GOTO EXIT_MODULE_INSTALLER
+
+if %module_name% == h GOTO HELP
+if %module_name% == help GOTO HELP
+
+Scripts\python -m pip uninstall %module_name%
+pause
+
+GOTO UNINSTALL_MODULE
+
+:HELP
+echo ScorpionIPX Python module uninstaller v 1.0
+echo.
+echo HELP MENU
+echo commands set
+echo h , help - Promts ScorpionIPX Python module uninstaller HELP MENU
+echo ex - Quits ScorpionIPX Python module uninstaller
+echo.
+GOTO UNINSTALL_MODULE
+
+:EXIT_MODULE_UNINSTALLER
+echo.
+echo Thank you for using ScorpionIPX Python module uninstaller v 1.0
+pause
